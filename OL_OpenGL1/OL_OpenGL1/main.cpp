3c6ae31b2864137096542c1ca0a63f84e27f6bb9
liyang <liyang@adnonstop.com>
2020-06-21 19:03:04 +0800

    添加源码

--- OL_OpenGL1/OL_OpenGL1/main.cpp
+++ OL_OpenGL1/OL_OpenGL1/main.cpp
@@ -2,170 +2,210 @@
 
 #include "GLTools.h"
 
+#include "GLFrame.h"
+
+#include "GLFrustum.h"
+
+#include "GLMatrixStack.h"
+
+
+#include "GLGeometryTransform.h"
+
+
 #include <glut/glut.h>
 
-GLBatch triangleBatch;
 
-GLShaderManager shaderManager;
+GLBatch    squareBatch;
+GLBatch greenBatch;
+GLBatch redBatch;
+GLBatch blueBatch;
+GLBatch blackBatch;
 
-GLfloat blockSize =  0.1f;
+GLShaderManager    shaderManager;
 
-GLfloat vVerts[] = {
-    -blockSize,blockSize,0,
-    blockSize,blockSize,0,
-    blockSize,-blockSize,0,
-    -blockSize,-blockSize,0
-};
 
-GLfloat xPos = 0.f;
-GLfloat yPos = 0.f;
+GLfloat blockSize = 0.2f;
+GLfloat vVerts[] = { -blockSize, -blockSize, 0.0f,
+                      blockSize, -blockSize, 0.0f,
+                      blockSize,  blockSize, 0.0f,
+                     -blockSize,  blockSize, 0.0f};
 
-//窗口大小改变时接受新的宽度和高度，其中0,0代表窗口中视口的左下角坐标，w，h代表像素
-
-void ChangeSize(int w,int h)
-
-{
-    
-    glViewport(0,0, w, h);
-    
-}
-
-//为程序作一次性的设置
-
+///////////////////////////////////////////////////////////////////////////////
+// This function does any needed initialization on the rendering context.
+// This is the first opportunity to do any OpenGL related tasks.
 void SetupRC()
-
-{
-    
-    //设置背影颜色
-    
-    glClearColor(0.0f,0.0f,1.0f,1.0f);
-    
-    //初始化着色管理器
+    {
+    // Black background
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f );
     
     shaderManager.InitializeStockShaders();
-    
-    //设置三角形，其中数组vVert包含所有3个顶点的x,y,笛卡尔坐标对。
-//
-//    GLfloat vVerts[] = {
-//
-//        -0.5f,-0.3f,0.0f,
-//
-//        0.5f,-0.3f,0.0f,
-//
-//        0.0f,0.5f,0.0f,
-//
-//    };
-    
-    //批次处理
-    
-    triangleBatch.Begin(GL_TRIANGLE_FAN,4);
-    
-    triangleBatch.CopyVertexData3f(vVerts);
-    
-    triangleBatch.End();
-    
-}
 
-//开始渲染
+    // Load up a triangle fan
+    squareBatch.Begin(GL_TRIANGLE_FAN, 4);
+    squareBatch.CopyVertexData3f(vVerts);
+    squareBatch.End();
 
-void RenderScene(void)
+    GLfloat vBlock[] = { 0.25f, 0.25f, 0.0f,
+                         0.75f, 0.25f, 0.0f,
+                         0.75f, 0.75f, 0.0f,
+                         0.25f, 0.75f, 0.0f};
+    
+    greenBatch.Begin(GL_TRIANGLE_FAN, 4);
+    greenBatch.CopyVertexData3f(vBlock);
+    greenBatch.End();
+    
 
-{
+    GLfloat vBlock2[] = { -0.75f, 0.25f, 0.0f,
+                          -0.25f, 0.25f, 0.0f,
+                          -0.25f, 0.75f, 0.0f,
+                          -0.75f, 0.75f, 0.0f};
+        
+    redBatch.Begin(GL_TRIANGLE_FAN, 4);
+    redBatch.CopyVertexData3f(vBlock2);
+    redBatch.End();
     
-    //清除一个或一组特定的缓冲区
-    
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
-    
-    //设置一组浮点数来表示红色
-    
-    GLfloat vRed[] = {1.0f,0.0f,0.0f,1.0f};
-    static GLfloat yRot = 0.f;
-    
-    // 结果矩阵，旋转矩阵，平移矩阵
-    M3DMatrix44f resultMatrix,rotationMatrix,tranformMatrix;
-    
-    m3dRotationMatrix44(rotationMatrix, yRot, 0, 0, 1);
-    m3dTranslationMatrix44(tranformMatrix, xPos, yPos, 0.0f);
-    m3dMatrixMultiply44(resultMatrix, rotationMatrix, tranformMatrix);
-    //传递到存储着色器，即GLT_SHADER_IDENTITY着色器，这个着色器只是使用指定颜色以默认笛卡尔坐标第在屏幕上渲染几何图形
-    
-    shaderManager.UseStockShader(GLT_SHADER_FLAT,resultMatrix,vRed);
-    
-    //提交着色器
-    
-    triangleBatch.Draw();
-    
-    //将在后台缓冲区进行渲染，然后在结束时交换到前台
-    
-    glutSwapBuffers();
-    
-}
 
-void SpecialKeys(int value,int x,int y) {
-   
-    GLfloat stepSize = 0.025f;
-    switch (value) {
-        case GL_KEYBORAD_UP:
-            <#statements#>
-            break;
-            
-        default:
-            break;
+    GLfloat vBlock3[] = { -0.75f, -0.75f, 0.0f,
+                        -0.25f, -0.75f, 0.0f,
+                        -0.25f, -0.25f, 0.0f,
+                        -0.75f, -0.25f, 0.0f};
+        
+    blueBatch.Begin(GL_TRIANGLE_FAN, 4);
+    blueBatch.CopyVertexData3f(vBlock3);
+    blueBatch.End();
+
+
+    GLfloat vBlock4[] = { 0.25f, -0.75f, 0.0f,
+                        0.75f, -0.75f, 0.0f,
+                        0.75f, -0.25f, 0.0f,
+                        0.25f, -0.25f, 0.0f};
+        
+    blackBatch.Begin(GL_TRIANGLE_FAN, 4);
+    blackBatch.CopyVertexData3f(vBlock4);
+    blackBatch.End();
     }
+
+// Respond to arrow keys by moving the camera frame of reference
+void SpecialKeys(int key, int x, int y)
+    {
+    GLfloat stepSize = 0.025f;
+
+    GLfloat blockX = vVerts[0];   // Upper left X
+    GLfloat blockY = vVerts[7];  // Upper left Y
+
+    if(key == GLUT_KEY_UP)
+        blockY += stepSize;
+
+    if(key == GLUT_KEY_DOWN)
+        blockY -= stepSize;
+    
+    if(key == GLUT_KEY_LEFT)
+        blockX -= stepSize;
+
+    if(key == GLUT_KEY_RIGHT)
+        blockX += stepSize;
+
+    // Collision detection
+    if(blockX < -1.0f) blockX = -1.0f;
+    if(blockX > (1.0f - blockSize * 2)) blockX = 1.0f - blockSize * 2;;
+    if(blockY < -1.0f + blockSize * 2)  blockY = -1.0f + blockSize * 2;
+    if(blockY > 1.0f) blockY = 1.0f;
+
+    // Recalculate vertex positions
+    vVerts[0] = blockX;
+    vVerts[1] = blockY - blockSize*2;
+    
+    vVerts[3] = blockX + blockSize*2;
+    vVerts[4] = blockY - blockSize*2;
+    
+    vVerts[6] = blockX + blockSize*2;
+    vVerts[7] = blockY;
+
+    vVerts[9] = blockX;
+    vVerts[10] = blockY;
+
+    squareBatch.CopyVertexData3f(vVerts);
+
     glutPostRedisplay();
-}
+    }
 
-int main(int argc,char* argv[])
 
-{
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Called to draw scene
+void RenderScene(void)
+    {
+    // Clear the window with current clearing color
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+    GLfloat vRed[] = { 1.0f, 0.0f, 0.0f, 0.5f };
+    GLfloat vGreen[] = { 0.0f, 1.0f, 0.0f, 1.0f };
+    GLfloat vBlue[] = { 0.0f, 0.0f, 1.0f, 1.0f };
+    GLfloat vBlack[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+
+    shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vGreen);
+    greenBatch.Draw();
+
+    shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed);
+    redBatch.Draw();
+
+    shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vBlue);
+    blueBatch.Draw();
+
+    shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vBlack);
+    blackBatch.Draw();
+
+
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed);
+    squareBatch.Draw();
+    glDisable(GL_BLEND);
     
-    //设置当前工作目录，针对MAC OS X
-    
+
+    // Flush drawing commands
+    glutSwapBuffers();
+    }
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Window has changed size, or has just been created. In either case, we need
+// to use the window dimensions to set the viewport and the projection matrix.
+void ChangeSize(int w, int h)
+    {
+    glViewport(0, 0, w, h);
+    }
+
+///////////////////////////////////////////////////////////////////////////////
+// Main entry point for GLUT based programs
+int main(int argc, char* argv[])
+    {
     gltSetWorkingDirectory(argv[0]);
     
-    //初始化GLUT库
-    
     glutInit(&argc, argv);
-    
-    /*初始化双缓冲窗口，其中标志GLUT_DOUBLE、GLUT_RGBA、GLUT_DEPTH、GLUT_STENCIL分别指
-     
-     双缓冲窗口、RGBA颜色模式、深度测试、模板缓冲区*/
-    
-    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH|GLUT_STENCIL);
-    
-    //GLUT窗口大小，标题窗口
-    
-    glutInitWindowSize(800,600);
-    
-    glutCreateWindow("Triangle");
-    
-    //注册回调函数
-    
-    glutReshapeFunc(ChangeSize);
-    
-    glutDisplayFunc(RenderScene);
-    
-    glutSpecialFunc(SpecialKeys);
-        
-    //驱动程序的初始化中没有出现任何问题。
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Move Block with Arrow Keys to see blending");
     
     GLenum err = glewInit();
-    
-    if(GLEW_OK != err) {
-        
-        fprintf(stderr,"glew error:%s\n",glewGetErrorString(err));
-        
+    if (GLEW_OK != err)
+        {
+        // Problem: glewInit failed, something is seriously wrong.
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
         return 1;
-        
-    }
+        }
     
-    //调用SetupRC
-    
-    SetupRC();
-    
-    glutMainLoop();
-    
-    return 0;
-    
-}
+    glutReshapeFunc(ChangeSize);
+    glutDisplayFunc(RenderScene);
+    glutSpecialFunc(SpecialKeys);
 
+    SetupRC();
+
+    glutMainLoop();
+    return 0;
+    }
