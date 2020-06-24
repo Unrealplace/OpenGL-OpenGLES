6c97057bbe4e718e3456d8db49c1e4e02e7052c5
李杨 <oliverleemail@gmail.com>
2020-06-24 10:53:52 +0800

    修改一下项目文件

---
+++ OpenGL_Demos/OpenGL_Demos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL_Demos
+//
+//  Created by mpm on 2020/6/24.
+//  Copyright © 2020 mpm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
